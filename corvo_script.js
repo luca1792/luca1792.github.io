4311338d98b74de65bb8e92bf8be495e3077e3ad
luca1792 <99828420+luca1792@users.noreply.github.com>
2022-03-16 22:54:36 +0100

    Update corvo_script.js

--- corvo_script.js
+++ corvo_script.js
@@ -1,1 +1,15 @@
-
+        function njump(){
+            pos.style.animation="";
+            zampasx.style.animation="top1 2s infinite";
+            zampadx.style.animation="top 2s infinite";
+        }
+        function jump(){
+            let pos=document.getElementById("pos");
+            let zampasx=document.getElementById("zampasx");
+            let zampadx=document.getElementById("zampadx");
+            pos.style.animation="jump 1s 1";
+            zampasx.style.animation="jumpz1 1s 1";
+            zampadx.style.animation="jumpz 1s 1";
+            myTo=setTimeout(njump,1000)
+        
+        }
